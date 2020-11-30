5c6b7f27a3c6c89e7447392aa5ce4daa876163ac
AlexandreGobe <73873959+AlexandreGobe@users.noreply.github.com>
2020-11-30 18:45:54 +0100

    php

---
+++ Gobé Alexandre.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+      <meta charset="utf-8">
+      <title>Gobé Alexandre</title>
+      <link rel="stylesheet" href="Gob%C3%A9%20Alexandre.css">
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+  </head>
+    
+  <body>
+    <div class="header-bg" style="background-image:url(noir.jpg)">
+        <div class="logo"><a href="#"></a></div>
+        <nav class="menu-nav">
+          <ul>
+            <li class="btn"><a href="#Home">Home</a></li>
+            <li class="btn"><a href="#A propos">A propos</a></li>
+            <li class="btn"><a href="#Compétences">Compétences</a></li>
+            <li class="btn"><a href="#Expérience">Expérience</a></li>
+            <li class="btn"><a href="#Formation">Formation</a></li>
+            <li class="btn"><a href="#Contact">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+      <main>
+          <div id="Home">
+              <br>
+        <h3> Gobé Alexandre </h3>
+        <p1>La vie de demain est entre nos mains.</p1>
+          <p class="photo1"><img src="moi1.png">
+          </p>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+        <p class="menuréseau">
+                <a href="https://fr.linkedin.com/" title="Linkedin">
+            <img alt="capture" src="gmail.png" />
+        </a>
+        <a href="https://www.instagram.com/?hl=fr" title="Instagram">
+            <img alt="insta" src="insta.png" />
+        </a>
+        <a href="https://fr-fr.facebook.com/" title="Facebook">
+            <img alt="FB" src="fb.png" />
+        </a>
+              </p>
+      
+    
+       
+    
+       <div id="A propos"> 
+           <br>
+           <br>
+        <h1>A propos</h1>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+       
+    <p class="txt">
+   <p5><strong>
+    Je suis étudiant en BTS Services Informatiques aux Organisations au pôle SupAvenir <br>
+    Saint Ursule qui se situe dans la ville de Caen.
+    </strong></p5>
+        <br>
+        <br>
+    <p5><strong>Passionné depuis mon jeune âge par le matériel informatique et son évolution, j'aime installer<br>
+    ces différentes machines, les configurer, les réparer mais également former les utilisateurs<br>
+    à se servir de ces derniers.
+    </strong></p5>
+        <br>
+        <br>
+    <p5><strong>
+    Je suis passionné, investi, minutieux et organisé afin d'accroitre mes compétences<br>
+    en informatique et de suivre l'évolution des différentes interfaces.
+    </strong></p5> 
+          </p>      
+    
+   
+   
+    
+    <br>  
+   <p class="photo2"><img src="apropos.png"></p>
+           <div id="Compétences">
+           <br>
+               <br>
+          
+          
+           <h1>Compétences</h1>
+<h2>Développement</h2>
+<br>
+<p4>Html</p4>
+<br>
+<div class="progress-barHTML" style="--width: 10" ></div>
+<br>
+<p4>CSS</p4>
+<br>
+<div class="progress-barCSS" style="--width: 10" ></div>
+<br>
+<p4>PHP</p4>
+<br>
+<div class="progress-barPHP" style="--width: 10" ></div>
+<br>
+<h2>Réseau</h2>
+<br>
+<p4>Cisco</p4>
+<br>
+<div class="progress-barCISCO" style="--width: 10" ></div>
+<br>
+<p4>Adressage IP</p4>
+<br>
+<div class="progress-barIP" style="--width: 10" ></div>
+              <div id="Expérience">
+<br>
+                  <br>
+          
+          <h1>Expérience</h1>  
+          <br>
+          <p> 
+         <img src="boulangerlogo.jpg">
+         <br>
+          <br>
+               - 2012/2017 :
+          </p>
+          <p> Boulangerie Gobé Christophe (aide) à Combourg.</p>
+          <p>- Entretien du magasin, du laboratoire, du poste de travail et du matériel</p>
+          <p>- Emballage des produits</p>
+          <p>- Cuisson du pain</p>
+          <p>- Aide au pâtissier et au boulanger ainsi qu'à la vente des produits</p>
+          <hr>
+          <p>
+        
+        <br>
+         <img src="gitemlogo.jpg">
+        <br>
+        <br>
+             - 2016 Décembre :</p>
+          <p>- Stage d’observation  Gitem à Combourg, une semaine.</p>
+          <p>- Inventaire de fin d’année, deux jours.</p>
+          <hr>
+          <p>
+        <br>
+        <img src="argentdepoche.png">
+        <br>
+        <br>
+            - 2018 Juillet/ Août : 
+          </p>
+          <p>Opération argent de poche    
+           pour la commune de Saint Quentin sur le Homme.</p>
+          <p>- Entretien des espaces verts</p>
+          <p>- Rénovation de bâtiments publics (peinture, papier peint)</p>
+          <p>- Rangement des stocks de fourniture</p>
+          <hr>
+          <p>
+            <br>
+        
+        <img src="carrefourmarketgrandparigny.png">
+        <br>
+        <br>
+             -2019 Juillet/Août :
+          </p>
+          <p> Employé libre service mise en rayon fruits et légumes,      
+              rayon frais ainsi que de la réalisation de commande drive à
+          </p> 
+          <p>
+              Carrefour Market Grandparigny.</p>
+          <p>- Mise en rayon de la marchandise</p>
+             <p>- Facing et Balisage</p>
+             <p>- Vérification de la DLC des produits</p>
+             <p>- Respect des règles d'hygiène et de sécurité</p>
+             <p>- Entretien du magasin, du poste de travail et du matériel</p>
+             <p>- Inventaire</p>
+             <p>- Emballage de produit </p>
+             <p>- Drive</p>
+          <hr>
+          <p>
+        <br>
+        <img src="logocarrefour.jpg">
+        <br>
+        <br>
+              
+               -2020 Juillet/Août : 
+          </p>
+          <p>Employé libre service mise en rayon fruits et légumes, à Carrefour 
+           Saint-Martin des champs, zone de la baie.</p>
+             <p>- Mise en rayon de la marchandise</p>
+             <p>- Facing et Balisage</p>
+             <p>- Vérification de la DLC des produits</p>
+             <p>- Respect des règles d'hygiène et de sécurité</p>
+             <p>- Entretien du magasin, du poste de travail et du matériel</p>
+             <p>- Inventaire</p>
+             <p>- Emballage de produit </p>
+          <br>
+          
+        <h1><a href="CV_Alexandre_Gob%C3%A9.pdf" download="CV Alexandre Gobé"> <strong>Télécharger Mon CV</strong></a></h1>
+              <div id="Formation">
+          <br>
+                  <br>
+          
+          
+           <h1>Formation</h1>
+        
+          <p>
+               <img src="college%20saint%20gilduin.png">
+       - 2013/2016 Collège saint-Gilduin à Combourg
+          </p>
+          <br>
+        <hr>
+        
+            <br>
+          <p>
+              <img src="notredamedelaprovidence.png">
+              - 2017 Collège Notre dame de la Providence à Avranches, 
+          Brevet Générale</p>
+        
+        <br>
+        <hr>
+      
+            <br>
+          <p>
+              <img src="lyc%C3%A9elamorandiere.png">
+              - 2017/2020 Lycée Julliot de la Morandière à Granville,
+          Bac Sciences et Technologies de l'Industrie et du Développement Durable.</p>
+        
+       <br>
+        <hr>
+       
+            <br>
+          <p><img src="supavenir.jpg">
+              - 2020/ à aujourd'hui SupAvenir Saint Ursule Caen, 
+              BTS Services Informatique aux Organisations.</p>
+       
+       
+        <br>
+      <div id="Contact"><h1>Contact</h1>
+        
+
+<div class="body_Contact">
+
+
+
+<table class="table_page">
+  <tr>
+    <td>
+      <form method="POST">
+              <p><label>Nom</label></p> <!--Etiquette de la case-->
+              <p><input type="text" name="nom"></p> <!--  -->
+              <p><label>Email</label></p>
+              <p><input type="email" name="email" required minlength="1"></p>
+              <p><label>Message</label></p>
+              <p><textarea id="message" name="message" cols="50" rows="10" required></textarea></p>
+              <div class="g-recaptcha" data-sitekey="6LekCeEZAAAAAGKY6QsC1ume5HZ3VB8HmFdBsyMX"></div>
+              <br/>
+              <input type="submit" id="bouton" name="bouton" value="Envoyez">
+      </form>
+
+
+<?php
+  require_once 'reCaptcha/autoload.php';
+  if(isset($_POST['bouton'])){
+       $nom = $_POST['nom'];
+       $email = $_POST['email'];
+       $message = $_POST['message'];
+       if(isset($_POST['g-recaptcha-response'])) {
+           $recaptcha = new \ReCaptcha\ReCaptcha('6LekCeEZAAAAANjTYWyqTj6rRM9svC6KtcxEDEQQ');
+           $resp = $recaptcha->verify($_POST['g-recaptcha-response']);
+       if ($resp->isSuccess()) {
+       echo "<center>Captcha Valide.</center>";
+
+       $entete  = 'MIME-Version: 1.0' . "\r\n";
+       $entete .= 'Content-type: text/html; charset="utf-8"' . "\r\n";
+       $entete .= 'From: ' . $email . "\r\n";
+       $message = '<h1>Message envoyé depuis la page Contact de site-personnel-fl.local</h1>
+       <p><b>Nom : </b>' . $nom . '<br>
+       <b>Email : </b>' . $email . '<br>
+       <b>Message : </b>' . $message . '</p>';
+       $retour = mail('haynewstek@gmail.com', 'Envoi depuis page Contact', $message, $entete);
+       if($retour)
+         echo '<center>Votre message a bien été envoyé.</center>';
+
+       } else {
+   $errors = $resp->getErrorCodes();
+   echo "<center>Captcha Invalide.</center>";
+  }
+  } else {
+  echo "<center>Captcha Non rempli.</center>";
+  }
+  }
+?>
+<footer>
+		<a href="Mentions%20L%C3%A9gales%20(1).html">Mentions légales </a>
+          <p class="lol"> 2020 © Tous droits réservés|Politique de confidentialité</p>
+      
+      </footer>
+
+
+    </body>
+</html>
+
+		
